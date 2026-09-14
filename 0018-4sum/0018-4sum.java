a4eac87e300cc9b37547c12cea791dcf94e23bf1
Eedebilli Sai Mohan <mohansai152006@gmail.com>
2026-09-14 15:28:57 +0530

    Time: 23 ms (13.68%), Space: 45.9 MB (46.51%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,49 +1,39 @@
 class Solution {
-    public int n=0;
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        n=nums.length;
+        int n=nums.length;
         Arrays.sort(nums);
-        return ksum(nums,4,target,0);
-    }
-    List<List<Integer>> ksum(int[] nums,int k, long target,int idx){
         List<List<Integer>> ans=new ArrayList<>();
-        if(idx>=n) return ans;
-        if(k==2){
-            int l=idx;
-            int r=n-1;
-            while(l<r){
-                long sum=(long)nums[l]+nums[r];
-                if(target==sum){
-                    List<Integer> sub=new ArrayList<>();
-                    sub.add(nums[l]);
-                    sub.add(nums[r]);
-                    ans.add(sub);
-                    while(l<n-1 && nums[l]==nums[l+1]) l++;
-                    while(r>l && nums[r]==nums[r-1]) r--;
-                    l++;
-                    r--;
-                }
-                else if(target>sum){
-                    while(l<n-1 && nums[l]==nums[l+1]) l++;
-                    l++;
-                }else{
-                    while(r>l && nums[r]==nums[r-1]) r--;
-                    r--;
-                }
-            }
-            return ans;
-        }else{
-            for(int i=idx;i<n-k+1;i++){
-                List<List<Integer>> sub=ksum(nums,k-1,target-nums[i],i+1);
-                if(sub.size()!=0){
-                    for(List<Integer> lst:sub){
-                        lst.add(0,nums[i]);
+        for(int i=0;i<n-3;i++){
+            for(int j=i+3;j<n;j++){
+                while(j<n-1 && nums[j]==nums[j+1]) j++;
+                long sub_target=(long)target-nums[i]-nums[j];
+                int l=i+1,r=j-1;
+                while(l<r){
+                    int sum=nums[l]+nums[r];
+                    if(sum==sub_target){
+                        List<Integer> sub=new ArrayList<>();
+                        sub.add(nums[i]);
+                        sub.add(nums[l]);
+                        sub.add(nums[r]);
+                        sub.add(nums[j]);
+                        ans.add(sub);
+                        while(l<n-1 && nums[l]==nums[l+1]) l++;
+                        while(r>0 && nums[r]==nums[r-1]) r--;
+                        l++;
+                        r--;
+                    }else if(sum>sub_target){
+                        while(r>0 && nums[r]==nums[r-1]) r--;
+                        r--;
+                    }else{
+                        while(l<n-1 && nums[l]==nums[l+1]) l++;
+                        l++;
                     }
-                    ans.addAll(sub);
                 }
-                while(i<n-1 && nums[i]==nums[i+1]) i++;
+                
             }
+            while(i<n-3 && nums[i]==nums[i+1]) i++;
         }
         return ans;
     }
+   
 }
